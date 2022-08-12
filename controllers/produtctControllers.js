d93dfc1e3c1f2e4513546f1697c85f1ead0c31ed
Franciane Menestrina <francianemenestrina@Francianes-MacBook-Pro.local>
2022-08-11 23:45:30 -0700

    Requisito 4

--- controllers/produtctControllers.js
+++ controllers/produtctControllers.js
@@ -14,14 +14,19 @@ const getProdutctsId = async (req, res) => {
 };
 
 const createProduct = async (req, res) => {
-   const { name } = req.body;
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+  if (name.length < 5) {
+    return res
+      .status(422)
+      .json({ message: '"name" length must be at least 5 characters long' });
+  }
   const result = await ProductsServices.createProduct(name);
-  // if (!result) return res.status(404).json({ message: "Product not found" });
   return res.status(201).json(result);
 };
 
-  module.exports = {
-    allProducts,
-    getProdutctsId,
-    createProduct,
-  };
+module.exports = {
+  allProducts,
+  getProdutctsId,
+  createProduct,
+};
